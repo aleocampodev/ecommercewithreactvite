0378964f942ce5b3e074d3310b26f39b621303b4
Aleja Ocampo <omaira.527@hotmail.com>
2024-07-01 20:45:21 -0500

    show list my order

--- src/pages/MyOrder/index.jsx
+++ src/pages/MyOrder/index.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { ShoppingCartContext } from "../../context";
 import Layout from '../../components/Layout'
+import OrderCard from "../OrderCard/index";
 
 const MyOrder = () => {
+  const {isCheckoutSideMenuOpen, closeCheckoutSideMenu, cartProducts, setCartProducts, setOrder, order, setCount} = useContext(ShoppingCartContext)
   return (
     <Layout>
       Hola my order
+      <div className=" flex flex-col w-80">
+      {
+        //show last product in cart
+        order.slice(-1)[0].products.map(product => {
+          return (
+            <OrderCard 
+              key={product.id}
+              title={product.title} 
+              imageUrl={product.image}
+              price={product.price}
+              id={product.id}
+            />
+          )
+        })
+      }
+      </div>
     </Layout>
   )
 }
