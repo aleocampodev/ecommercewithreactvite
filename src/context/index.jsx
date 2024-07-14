98ee3da3c12ca05b9aae868f3f25e077ddeca6c5
Aleja Ocampo <omaira.527@hotmail.com>
2024-07-13 23:48:44 -0500

    search products

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { api } from '../api'
 
 export const ShoppingCartContext = createContext()
 
@@ -10,6 +11,27 @@ export const ShoppingCartProvider = ({ children }) => {
   const [productShow, setProductShow] = useState({})
   const [cartProducts, setCartProducts] = useState([])
   const [order, setOrder] = useState([])
+  const [products, setProducts] = useState(null)
+  const [filteredProducts, setFilteredProducts]= useState(null)
+  const [searchByTitle, setSearchByTitle] = useState(null)
+
+  useEffect(() => {
+    fetch(`${api}/products`)
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  },[])
+
+
+
+  console.log('filteredItems', filteredProducts)
+
+  const filteredProductByTitle = (products, searchByTitle) => {
+    return products?.filter(product => product.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+  }
+
+  useEffect(()=> {
+    if(searchByTitle) setFilteredProducts(filteredProductByTitle(products, searchByTitle))
+  },[products,searchByTitle])
 
   const openProductDetail = () =>  setIsProductDetailOpen(true)
   const closeProductDetail = () => setIsProductDetailOpen(false)
@@ -34,7 +56,12 @@ export const ShoppingCartProvider = ({ children }) => {
       openCheckoutSideMenu,
       closeCheckoutSideMenu,
       order, 
-      setOrder
+      setOrder,
+      products,
+      setProducts,
+      searchByTitle,
+      setSearchByTitle,
+      filteredProducts
     }}>
       {children}
     </ShoppingCartContext.Provider>
